b3c3db7831298897ec4d5b073573e2c2475f7871
Haruto Ikehara <kduxwr@gmail.com>
2025-01-10 11:35:29 +0900

    Fix auth routes

--- report_app/api/config/routes.rb
+++ report_app/api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth", controllers: {
-    registrations: "v1/auth/registrations"
-  }
+  namespace :v1 do
+    mount_devise_token_auth_for "User", at: "auth", controllers: {
+      registrations: "v1/auth/registrations"
+    }
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
