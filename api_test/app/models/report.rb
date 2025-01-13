86efa6edb9d8393054d5ffafb5e03404a5b2975f
Haruto Ikehara <kduxwr@gmail.com>
2025-01-14 07:56:22 +0900

    Remove api_tests

--- api_test/app/models/report.rb
+++
@@ -1,4 +0,0 @@
-class Report < ApplicationRecord
-  validates :date, presence: true, uniqueness: true
-  validates :content, presence: true
-end
--- api_test/spec/factories/reports.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :report do
-    sequence(:date) { |n| Date.today + n.days }
-    sequence(:content) { |n| "Sample report content ##{n}" }
-  end
-end
